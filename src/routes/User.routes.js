cc4978bdae4df9c5f3ad677d08ee70e95087855a
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-11-27 12:07:40 +0100

    API: Creating routes and configuring it

---
+++ src/routes/User.routes.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+const userRoutes = Router();
+
+const UserController = require('../controllers/UserController');
+const userController = new UserController();
+
+userRoutes.post('/', userController.create);
+
+module.exports = userRoutes;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
-const PORT = 3000;
 
+app.use(express.json());
+
+app.use(routes);
+
+
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
