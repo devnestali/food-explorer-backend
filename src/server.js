7e22ee42765d979bdaac3dfb19ed57ec2f8d8d34
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-11-27 15:01:12 +0100

    API: Creating AppError file to handle errors around the requisitions

--- src/server.js
+++ src/server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const routes = require('./routes');
 
+const AppError = require('./utils/AppError');
+
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
 
+app.use((error, request, response, next) => {
+    if (error instanceof AppError) {
+        response.status(error.statusCode).json({
+            status: "error",
+            message: error.message,
+        });
+    }
+
+    response.status(500).json({
+        status: "error",
+        message: "Ocorreu um erro interno. Por favor, entre em contato com o responsável."
+    });
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
