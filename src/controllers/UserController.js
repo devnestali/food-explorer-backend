006a5470d73f853a157317e46cec1425e8cc730e
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-11-28 12:04:26 +0100

    API: Creating create method on UserController

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,19 +1,35 @@
 const knex = require('../database');
+const bcrypt = require('bcrypt');
 
-
-const VerifyData = require('../utils/VerifyData');
+const VerifyData = require('../utils/VerifyData.js');
 const verifyData = new VerifyData();
 
 class UserController {
-    create(request, response) {
+    async create(request, response) {
         const { username, email, password } = request.body;
 
-        verifyData.usersInfoExists({ username, email, password });
+        const emptyField = !username || !email || !password;
+        
+        verifyData.usersInfoExists(emptyField);        
+
+        const emailExists = await knex("users").where({ email }).first();
+        
+        verifyData.userAlreadyExists(emailExists);
+
+        
+        const formattedUsername = username.trim();
+        const formattedEmail = email.trim();
+        const hashedPassword = bcrypt.hashSync(password, 8);
+
+        await knex("users").insert({
+            username: formattedUsername,
+            email: formattedEmail,
+            password: hashedPassword
+        });
 
         return response.status(200).json({
-            username,
-            email,
-            password
+            username: formattedUsername,
+            email: formattedEmail,
         }); 
     };
 };   
