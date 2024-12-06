e623ddc3a45684fffb9df91f223a577766023766
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-12-06 10:21:50 +0100

    WEB: Creating config to JWT and creating token to user

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -5,9 +5,13 @@ const { compare } = require('bcrypt');
 
 const knex = require('../database');
 
+const { sign } = require('jsonwebtoken');
+const authConfig = require('../configs/auth');
+
 class SessionController {
     async create(request, response) {
         const { email, password } = request.body;
+        const { expiresIn, secret } = authConfig.jwt;
 
         const emptyfield = !email || !password;
         verifyData.userInfoExists(emptyfield);
@@ -18,8 +22,16 @@ class SessionController {
         const passwordMatch = await compare(password, user.password);
         verifyData.sessionPasswordVerification({ password: passwordMatch })
         
+        const payload = { id: user.id };
 
-        return response.status(200).json(user);
+        const token = sign(payload, secret, { expiresIn });
+        
+        const authorizedUser = {
+            ...user,
+            token
+        };
+
+        return response.status(200).json(authorizedUser);
     };
 };
 
