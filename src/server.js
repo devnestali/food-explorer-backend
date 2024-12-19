690daa8536f887528a3acd9f89775f75c153c2e6
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-12-19 09:30:21 +0100

    API: Adding cors to server file

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('express-async-errors');
 
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 const path = require('path');
 const multerConfig = require('./configs/multerConfig');
@@ -9,12 +10,14 @@ const multerConfig = require('./configs/multerConfig');
 const AppError = require('./utils/AppError');
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
 app.use(express.static(multerConfig.UPLOAD_FOLDER));
 
 app.use(routes);
+
 
 
 app.use((error, request, response, next) => {
